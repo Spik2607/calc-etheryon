07ab73a6891ad8f7827e7cec03df6061cdc90727
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-05 16:49:10 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,14 @@ import './globals.css';
 
 export const metadata = {
   title: 'Calculatrice Etheryon',
-  description: 'Calculateur de points pour le jeu Etheryon',
+  description: 'Calculatrice de points pour le jeu Etheryon',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
-      <body>
+    <html lang="fr" suppressHydrationWarning>
+      <head />
+      <body suppressHydrationWarning>
         {children}
       </body>
     </html>
