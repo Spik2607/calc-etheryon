839e6df2f27429fd0c4c015e491e2ed02d1f3b61
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-05 15:16:22 +0000

    Fresh start with working configuration

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import EtheryonCalculator from '@/components/EtheryonCalculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <EtheryonCalculator />
+    </main>
+  )
+}
