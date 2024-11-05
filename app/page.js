f163e81dff310e204eec4582fb12f1a432b14cdb
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-05 16:48:55 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 'use client';
 
-import EtheryonCalculator from '@/components/EtheryonCalculator';
+import EtheryonCalculator from '../components/EtheryonCalculator';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <div className="container mx-auto p-4">
-        <EtheryonCalculator />
-      </div>
-    </div>
+    <main className="min-h-screen">
+      <EtheryonCalculator />
+    </main>
   );
 }
