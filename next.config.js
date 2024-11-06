97f1a7b75a2759302b48dd71a978c268bc675efd
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-06 16:46:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/calc-etheryon' : '',
+  basePath: '/calc-etheryon',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
