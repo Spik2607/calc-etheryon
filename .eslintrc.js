bca68ad8f514118af54def3c5724b81ee4f26250
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-07 13:03:28 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: ['next', 'next/core-web-vitals'],
+  extends: "next/core-web-vitals",
   rules: {
-    'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'no-unused-vars': 'off'
+    "@typescript-eslint/no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-expressions": "off",
+    "no-unused-vars": "off"
   }
 };
