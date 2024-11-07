e68aa85f143667b080e9db2a839ddf8c301ca209
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-07 12:48:42 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-'use client';
-
 export default function Home() {
-  const EtheryonCalculator = require('../components/EtheryonCalculator').default;
-  
   return (
-    <div className="min-h-screen">
-      <EtheryonCalculator />
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
