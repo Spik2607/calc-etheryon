59e07950d2be22f918777044c2b4ef911a2ed2f2
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 16:29:39 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import '../styles/globals.css'
-import { Inter } from 'next/font/google'
-
-// Si vous utilisez une police personnalisée, configurez-la ici
-const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${inter.className} min-h-screen bg-background`}>
+    <div className="min-h-screen bg-background">
       <Component {...pageProps} />
-    </main>
+    </div>
   )
 }
