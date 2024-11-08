8899018049e72840c7fa7902a3f2c7c8d2e7130a
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 17:23:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,10 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <Card className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Calculatrice Etheryon</h1>
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white dark:from-gray-900 dark:to-gray-800">
+      <div className="container mx-auto p-4">
         <EtheryonCalculator />
-      </Card>
+      </div>
     </div>
   )
 }
