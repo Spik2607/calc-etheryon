c14a68ebfcb0c009c139755928aae16733e9557c
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 17:04:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',  // Crucial pour GitHub Pages
-  basePath: '/calc-etheryon', // Le nom de votre repo
+  output: 'export',
+  basePath: '/calc-etheryon',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
