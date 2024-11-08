f700f36d3fba43ebc50dbb2b640d0c9f13bb55ae
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 16:33:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import EtheryonCalculator from '@/components/calculator/EtheryonCalculator'
 import { Card } from '@/components/ui/card'
+import React from 'react'
+import { Input } from '@/components/ui/input'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
