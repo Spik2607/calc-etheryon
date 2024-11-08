8556d12e7072a148b7c534e912b650f239ca5d53
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 16:24:49 +0000

    Deploying to gh-pages from @ Spik2607/calc-etheryon@8899018049e72840c7fa7902a3f2c7c8d2e7130a 🚀

--- _next/static/dPF8aE9lVdcqVfml5lNyZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/611-aa38067214fcbf78.js","static/chunks/pages/index-cdf1d159883919d9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
