80b68f7236db584b81829c864a97bcad20aa7174
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-08 16:16:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import EtheryonCalculator from '@/components/calculator/EtheryonCalculator'
+import { Card } from '@/components/ui/card'
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Calculatrice Etheryon</h1>
-      {/* Votre contenu ici */}
+      <Card className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Calculatrice Etheryon</h1>
+        <EtheryonCalculator />
+      </Card>
     </div>
   )
 }
