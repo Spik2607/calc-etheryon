16e36283a4f4d131675767db72a81cd1a4278414
Spik2607 <Spik2607@users.noreply.github.com>
2024-11-08 15:47:44 +0000

    deploy: 17751190c918cc0875b96e65a673e80465c7277c

---
+++ _next/static/chunks/pages/_app-dfcd99afd8e2dbe3.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{1118:function(n,r,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(6004)}])},6004:function(n,r,e){"use strict";e.r(r),e.d(r,{default:function(){return o}});var t=e(5893);function o(n){let{Component:r,pageProps:e}=n;return(0,t.jsx)("div",{className:"min-h-screen bg-background",children:(0,t.jsx)(r,{...e})})}e(3814)},3814:function(){},5251:function(n,r,e){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=e(7294),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,_=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(n,r,e){var t,u={},i=null,s=null;for(t in void 0!==e&&(i=""+e),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(s=r.ref),r)f.call(r,t)&&!c.hasOwnProperty(t)&&(u[t]=r[t]);if(n&&n.defaultProps)for(t in r=n.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:n,key:i,ref:s,props:u,_owner:_.current}}r.Fragment=u,r.jsx=i,r.jsxs=i},5893:function(n,r,e){"use strict";n.exports=e(5251)}},function(n){var r=function(r){return n(n.s=r)};n.O(0,[179],function(){return r(1118),r(6885)}),_N_E=n.O()}]);
