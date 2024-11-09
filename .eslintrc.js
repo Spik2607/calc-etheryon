988e641ad8ca4afda2458ceadfefc5c268d06d60
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-09 23:43:44 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     "@typescript-eslint/no-unused-expressions": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "no-unused-expressions": "off",
-    "no-unused-vars": "off"
-  }
+    "no-unused-vars": "off",
+    "react/no-unescaped-entities": "off",
+    "react/display-name": "off",
+    "react/jsx-no-undef": "off",
+    "react/prop-types": "off"
+  },
+  ignorePatterns: ["**/*.test.js", "**/*.spec.js"]
 };
