34e24a55e3430cc964ba4587cf9098e3c1735129
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-09 10:59:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
+import { ThemeProvider } from 'next-themes'
+import { AnimatePresence } from 'framer-motion'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-background">
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider 
+      attribute="class" 
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <AnimatePresence mode="wait">
+        <div className="min-h-screen bg-background font-sans antialiased">
+          <div className="relative flex min-h-screen flex-col">
+            <main className="flex-1">
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </div>
+      </AnimatePresence>
+    </ThemeProvider>
   )
 }
