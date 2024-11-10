60f6579fa3a720fc038c1010f305fd729543e68e
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-11-10 12:23:17 +0000

    Deploying to gh-pages from @ Spik2607/calc-etheryon@ba8d92d820052f2346d7b0f739db962cc4e31815 🚀

---
+++ _next/static/XKRGK_onjDtJYU8vu8Ydl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/26-eb2bc923688cf024.js","static/chunks/pages/index-0b250f72de235267.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/z0DvIs7RzUPqJ0DAfDNNQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/30-679be79d1389cbc2.js","static/chunks/pages/index-3240e4d4c6f4ade7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
